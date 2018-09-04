9e7f601587a4e2fc59dfb348521be75df7718ece
Maze7 <marcel-maze7@hotmail.de>
2018-09-04 14:44:55 +0200

    first design of playfield class

---
+++ src/Playfield.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "Tetris.cpp"
+#include "Typedefs.h"
+#include "Tetromino.h"
+
+namespace TetrisGame {
+	class Playfield
+	{
+	public:
+		static constexpr int s_ROWS = 22;
+		static constexpr int s_COLUMNS = 10;
+		static constexpr int s_OFFSET = 10;
+
+		const uint m_WIDTH{ TetrisGame::getWindow()->getSize().x };
+		const uint m_HEIGHT{ TetrisGame::getWindow()->getSize().y };
+
+	private:
+
+		std::vector<std::vector<sf::Color>> m_grid;
+		uint m_BLOCK_SIZE{ (m_HEIGHT - 2 * s_OFFSET) / s_COLUMNS };
+
+	public:
+		Playfield();
+		~Playfield();
+
+		std::vector<int> checkForCompletedRows();
+		void deleteRow(uint row);
+		void draw(TetrisGame::Tetromino& tetromino);
+
+	};
+}
