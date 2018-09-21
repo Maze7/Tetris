6104d5641524a83db0b37b3d59bf6108c4898d03
Maze7 <marcel-maze7@hotmail.de>
2018-09-21 17:49:54 +0200

    new Collection class with run method

---
+++ src/GameCollection/Collection.cpp
@@ -0,0 +1,78 @@
+#include "Collection.h"
+#include <iostream>
+
+std::map<std::string, std::vector<GameCollection::ICollectionEntry*>*> GameCollection::Collection::s_entrys = std::map<std::string, std::vector<GameCollection::ICollectionEntry*>*>();
+
+
+/*
+	This is the main method of GameCollection
+*/
+int GameCollection::Collection::run(sf::RenderWindow& window, sf::Font& font)
+{
+	sf::Event event;
+	MenuEntry mainMenu;
+	ICollectionEntry* currentScreen;
+
+	// Helpers
+	std::vector<ICollectionEntry*>* entrysOfModule;
+	int screenIndex = 0;
+
+	currentScreen = &mainMenu;
+	while (window.isOpen()) {
+
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) 
+				window.close();
+			else if (event.type == sf::Event::KeyPressed) 
+				currentScreen->handleEvent(event);
+		}
+
+		if (!currentScreen->isRunning()) {
+			screenIndex = currentScreen->close();
+			switch (screenIndex)
+			{
+			case currentScreen->EXIT_APP:
+				window.close();
+				return EXIT_SUCCESS;
+				break;
+
+			case currentScreen->MAIN_MENU:
+				currentScreen = &mainMenu;
+				currentScreen->setRunning();
+				break;
+
+			default:
+				entrysOfModule = s_entrys.at(mainMenu.getCurrentName());
+				currentScreen = entrysOfModule[0][screenIndex];
+				currentScreen->setRunning();
+			}
+		}
+
+		currentScreen->handleTime();
+		window.clear();
+		currentScreen->draw(&window, &font);
+		window.display();
+	}
+	return EXIT_FAILURE;
+}
+
+/*
+	Static method is needed to load Screens/entrys. 
+	A modul loader can store screens with a matching name inside s_entrys. 
+
+	Param:moduleName specify the name of the hole modul. Each modul can have n screens which
+	are stored in param:screen 
+	It is recommended to set modulName equals to the used namespace. 
+*/
+void GameCollection::Collection::addModuleEntrys(std::string moduleName, std::vector<GameCollection::ICollectionEntry*>* screens)
+{
+	s_entrys.insert(std::make_pair(moduleName, screens));
+}
+
+/*
+	Getter for s_screens. Returns a pointer to hole map of std::vector<ICollectionEntrys*>*
+*/
+const std::map<std::string, std::vector<GameCollection::ICollectionEntry*>*>* const GameCollection::Collection::getEntrys()
+{
+	return &s_entrys;
+}
--- src/GameCollection/GameCollection.cpp
+++ src/GameCollection/GameCollection.cpp
@@ -1,6 +1,19 @@
-#include "Tetris/TetrisGame.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Collection.h"
 
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(1200, 800), "GameCollection");
+	sf::Font font;
 
+	// Todo ioutils
+	if (!font.loadFromFile("big_noodle_titling.ttf")) {
+		std::cerr << "[ERROR] [MAIN] Could not load font" << std::endl;
+		return 1;
+	}
+
+	srand(time(NULL));
+	GameCollection::Collection col;
+	return col.run(window, font);
 }
