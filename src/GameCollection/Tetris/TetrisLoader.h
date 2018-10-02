2e8dfdac4c3f61fb4cd3ea832a30193864b39089
Maze7 <marcel-maze7@hotmail.de>
2018-10-02 19:15:05 +0200

    load scorescreen

--- src/GameCollection/Tetris/TetrisLoader.h
+++ src/GameCollection/Tetris/TetrisLoader.h
@@ -4,6 +4,7 @@
 #include "TetrisMenu.h"
 #include "SettingsMenu.h"
 #include "Game.h"
+#include "ScoreScreen.h"
 #include <string>
 
 namespace TetrisGame
@@ -14,11 +15,12 @@ namespace TetrisGame
 		Game game = Game();
 		TetrisMenu menu = TetrisMenu();
 		SettingsMenu settings = SettingsMenu();
+		ScoreScreen score = ScoreScreen();
 
-		std::vector<GameCollection::ICollectionEntry*> modulScreens = {&menu/*0*/, &game/*1*/ , &settings/*2*/};
+		std::vector<GameCollection::ICollectionEntry*> modulScreens = {&menu/*0*/, &game/*1*/ , &settings/*2*/, &score/*3*/};
 	public:
 		static constexpr char MODUL_NAME[] = "Tetris";
-		enum SCREENS { MENU=0, GAME=1, SETTINGS=2 };
+		enum SCREENS { MENU=0, GAME=1, SETTINGS=2, SCORE=3};
 
 		TetrisLoader();
 	};
