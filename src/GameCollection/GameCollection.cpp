6cc20d9fcd2ebb0c6f1cf0d0dc2f8f2ccb32f2d0
Maze7 <marcel-maze7@hotmail.de>
2018-10-10 18:45:26 +0200

    moved font to static dir

--- src/GameCollection/GameCollection.cpp
+++ src/GameCollection/GameCollection.cpp
@@ -9,7 +9,7 @@ int main()
 	sf::Font font;
 
 	// Todo ioutils
-	if (!font.loadFromFile("big_noodle_titling.ttf")) {
+	if (!font.loadFromFile("static/big_noodle_titling.ttf")) {
 		std::cerr << "[ERROR] [MAIN] Could not load font" << std::endl;
 		return 1;
 	}
